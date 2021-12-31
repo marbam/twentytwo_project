cf45c4ef70a16c6cabe8610a750e6df021c92da3
Martin Bampton <martinbampton@hotmail.com>
2021-12-31 23:43:17 +0000

    Adding APPROVED field to users, removing register functionality.

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
 Route::get('/', function () {
     return view('welcome');
-});
+})->middleware(['approved']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
