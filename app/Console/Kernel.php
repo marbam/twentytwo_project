1626a51ae2063b5de626d3365fe7a687f58eab78
Martin Bampton <martinbampton@hotmail.com>
2022-04-30 13:09:37 +0100

    Call and populate Ecologi data

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,7 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use App\Http\Controllers\CronController;
 
 class Kernel extends ConsoleKernel
 {
@@ -15,7 +16,9 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->call(function () {
+            (new CronController)->callEcologiAPI();
+        })->monthly();
     }
 
     /**
---
+++ app/Http/Controllers/CronController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Log;
+use Auth;
+use App\Models\Setting;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+class CronController extends Controller
+{
+
+    public function callEcologiAPI() {
+
+        Log::debug('Retrieving Ecologi Data...');
+
+        $data = json_decode(file_get_contents('https://public.ecologi.com/users/martinbampton/impact'), true);
+        if ($data['trees']) {
+            Setting::firstOrCreate(
+                ['key' => 'number_trees'],
+                ['value' => $data['trees']],
+            );
+        }
+
+        if ($data['carbonOffset']) {
+            Setting::firstOrCreate(
+                ['key' => 'carbon_offset'],
+                ['value' => $data['carbonOffset']],
+            );
+        }
+
+        Log::debug('Ecologi Data Retrieved...');
+    }
+
+}
